f7f95885ff1b74935131dee9a446ab386d01e33c
karthkk <karthkk@gmail.com>
2017-03-13 21:30:25 -0700

    Fixed issues with list append and path

--- detection_models/model_conf.py
+++ detection_models/model_conf.py
@@ -3,13 +3,13 @@ import os
 from model_containers import Model
 model_output_folder = ''
 this_dir = os.path.dirname(__file__)
-model_location = this_dir + '../output/faster_rcnn_end2end/%s'
+model_location = this_dir + '/../output/faster_rcnn_end2end/%s'
 
 
 
 def setup(sess):
     models = []
-    models.add(Model("office_supplies", ('__background__',
+    models.append(Model("office_supplies", ('__background__',
                                          "box",
                                          "gum",
                                          "marker",
@@ -19,7 +19,7 @@ def setup(sess):
                                          "tape",
                                          "usb"
     ), model_location%"office_supplies/VGGnet_fast_rcnn_iter_200.ckpt", sess))
-    models.add(Model("arm", ('__background__',
+    models.append(Model("arm", ('__background__',
                                          "arm"
     ), model_location%"armpos/VGGnet_fast_rcnn_iter_2000.ckpt", sess))
 
